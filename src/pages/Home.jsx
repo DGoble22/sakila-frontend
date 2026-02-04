563694d4eda9c314f2a94caf938f6d4cdae52138
Dylan Goble <dgoble0516@gmail.com>
2026-02-04 13:42:40 -0500

    feat: connected top-films api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+    const [topFilms, setTopFilms] = useState([]);
+    const [loadingTopFilms, setLoadingTopFilms] = useState(true);
+
+    useEffect(() => {
+        const fetchTopFilms = async () => {
+            try {
+                const response = await fetch('/api/top-films');
+                if (!response.ok) {
+                    throw new Error('Bad Network Response');
+                }
+                const data = await response.json();
+                setTopFilms(data);
+            } catch (error) {
+                console.error('Error fetching top films:', error);
+            } finally {
+                setLoadingTopFilms(false);
+            }
+        };
+
+        fetchTopFilms()
+    }, []);
+
+    if (loadingTopFilms) {return <p>Loading top films...</p>;}
+
     return (
         <div>
             <h1>Landing Page</h1>
+            <h2>Top 5 Rented Films</h2>
+            <ul>
+                {topFilms.map((film) => (
+                    <li key={film.id}>{film.title}</li>
+                ))}
+            </ul>
         </div>
     );
 }
