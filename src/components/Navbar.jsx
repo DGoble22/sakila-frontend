e9ac4f2e84c70e852d70571ce616aaeaf1e5494e
Dylan Goble <dgoble0516@gmail.com>
2026-02-09 20:37:04 -0500

    feat: implemented Top 5 actors table with details modal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,37 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand navbar-dark bg-dark shadow-sm py-3">
-            <div className="container">
-                {/* Logo */}
-                <Link to="/" className="navbar-brand fw-bold">SAKILA</Link>
+    const location = useLocation();
 
-                {/* Navbar Links */}
+    return (
+        <nav className="navbar navbar-expand navbar-dark navbar-gradient shadow-lg">
+            <div className="container-fluid px-4">
+                {/* Sakila Logo */}
+                <Link to="/" className="navbar-brand fw-bold fs-3">SAKILA</Link>
+
+                {/* Navbar center links */}
                 <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mx-auto">
+                    <ul className="navbar-nav mx-auto gap-2">
                         <li className="nav-item">
-                            <Link to="/" className="nav-link px-3">Home</Link>
+                            <Link to="/" className={`nav-link px-4 py-2 rounded-pill fw-semibold ${location.pathname === '/' ? 'active' : ''}`}>
+                                Home
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/Customer" className="nav-link px-3">Customer</Link>
+                            <Link to="/Customer" className={`nav-link px-4 py-2 rounded-pill fw-semibold ${location.pathname === '/Customer' ? 'active' : ''}`}>
+                                Customers
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/Films" className="nav-link px-3">Films</Link>
+                            <Link to="/Films" className={`nav-link px-4 py-2 rounded-pill fw-semibold ${location.pathname === '/Films' ? 'active' : ''}`}>
+                                Films
+                            </Link>
                         </li>
                     </ul>
                 </div>
-                <div style={{ width: "80px" }} className="d-none d-lg-block"></div>
+                <div style={{ width: "100px" }} className="d-none d-lg-block"></div>
             </div>
         </nav>
     );
